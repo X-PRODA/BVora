fa1d2c4db430e0bb91f93c782109b80b177f2477
X-PRODA <90550712+X-PRODA@users.noreply.github.com>
2023-01-31 18:53:59 +0100

    Update BVora.py

--- BVora.py
+++ BVora.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as mp
+import matplotlib as mp
 import numpy as np
 import time as tm
 import pygame as pg
@@ -6,6 +6,44 @@ import tkinter as tk
 import maths as mt
 import os
 import random as rd
+import request as rq
+import scipy as sp
+def evr(iterable):
+    all(iterable)
+def ning(iterable):
+    any(iterable)
+def crear(titulo, extensión):
+    with open(titulo + "." + archivo, "x") as e
+def codificar(archivo, codificacion):
+    with open(archivo, "rb") as e
+    with open(archivo, "wb") as f
+    f.write(e.read() * (10 ** codificacion))
+def codavanze(archivo, num1, num2):
+    with open(archivo, "rb") as e
+    with open(archivo, "wb") as f
+    num = rd.randint(10 ** (num1 ** num2)/num1 - num2)
+    f.write(e.read() * num)
+    with open("key.txt", "x") as g
+    with open("key.txt", "w") as g
+    g.write(num)
+    g.close()
+def decodeavanze(archivo):
+    with open("key.txt", "r") as e
+    with open(archivo, "wb") as f
+    with open(archivo, "rb") as g
+    f.write(g.read() / e.read())
+    f.close()
+def decode(archivo, codificación):
+    with open(archivo, "wb") as f
+    with open(archivo, "rb") as g
+    f.write(g.read() / (10 ** codificación))
+    f.close()
+def out(string):
+    print(string)
+def inp(string):
+    return input(string)
+def ejec(comando):
+    os.system(comando)
 with open(input("archivo"), "r") as a
 with compile(a.read(), "BV", "exec") as b
 exec(b)
