d63ced92b1fec9c7eb42f6868cb510c2ab30ed32
X-PRODA <90550712+X-PRODA@users.noreply.github.com>
2023-02-03 17:13:42 +0100

    Add files via upload

--- BVeditor.py
+++ BVeditor.py
@@ -1,24 +1,33 @@
-o = input("--create//-/-//open--  ")
+from os import remove
 a = 0
 l = 0
 while a == 0:
+    o = input("--create//-/-//open//-/-//delete--  ")
     if o == "create":
         n = input("name - ")
         open("proyects/" + n + ".bv", "x")
         a = a + 1
     elif o == "open":
         n = input("name - ")
-        with open("proyects/" + n + ".bv", "r") as f
+        f = open("proyects/" + n + ".bv", "r")
         print(f.read())
         a = a + 1
+    elif o == "delete":
+        n = input("name - ")
+        remove("proyects/" + n + ".bv")
+    elif o == "config":
+        n = input("configuration - ")
+        f = open("changefiles/" + n, "r")
+        print(f.read())
+        f = open("changefiles/" + n, "w")
+        f.write(input("texto - "))
+    elif o == "funtions":
+        f = open("changefiles/funtions", "r")
     else:
         print("I don't understand you. Can yo repeat?")
-l = l + 1
-with open("proyectos/" + n + ".bv", "a") as f
-f.write(input(str(l) + " - "))
-f.close()
 while True:
     l = l + 1
-    with open("proyectos/" + n + ".bv", "a") as f
-    f.write("/n" + input(str(l) + " - "))
+    f = open("proyects/" + n + ".bv", "a")
+    f.write(input(str(l) + " - "))
+    f.write("\n")
     f.close()
--- BVora.py
+++ BVora.py
@@ -1,43 +1,37 @@
-import matplotlib as mp
-import numpy as np
 import time as tm
-import pygame as pg
 import tkinter as tk
-import maths as mt
 import os
 import random as rd
-import request as rq
-import scipy as sp
-with open("changefiles/intext.bvm", "r") as d
+d = open("changefiles/intext.bvm", "r")
 print(d.read())
 def evr(iterable):
     all(iterable)
 def ning(iterable):
     any(iterable)
 def crear(titulo, extensión):
-    with open(titulo + "." + archivo, "x") as e
+    e = open(titulo + "." + archivo, "x")
 def codificar(archivo, codificacion):
-    with open(archivo, "rb") as e
-    with open(archivo, "wb") as f
+    e = open(archivo, "rb")
+    f = open(archivo, "wb")
     f.write(e.read() * (10 ** codificacion))
 def codavanze(archivo, num1, num2):
-    with open(archivo, "rb") as e
-    with open(archivo, "wb") as f
+    e = open(archivo, "rb")
+    f = open(archivo, "wb")
     num = rd.randint(10 ** (num1 ** num2)/num1 - num2)
     f.write(e.read() * num)
-    with open("key.txt", "x") as g
-    with open("key.txt", "w") as g
+    open("key.txt", "x")
+    q = open("key.txt", "w")
     g.write(num)
     g.close()
 def decodeavanze(archivo):
-    with open("key.txt", "r") as e
-    with open(archivo, "wb") as f
-    with open(archivo, "rb") as g
+    e = open("key.txt", "r")
+    f = open(archivo, "wb")
+    g = open(archivo, "rb")
     f.write(g.read() / e.read())
     f.close()
 def decode(archivo, codificación):
-    with open(archivo, "wb") as f
-    with open(archivo, "rb") as g
+    f = open(archivo, "wb")
+    g = open(archivo, "rb")
     f.write(g.read() / (10 ** codificación))
     f.close()
 def out(string):
@@ -46,9 +40,8 @@ def inp(string):
     return input(string)
 def ejec(comando):
     os.system(comando)
-with open(input("archivo"), "r") as a
-with compile(a.read(), "BV", "exec") as b
-exec(b)
-with open("changefiles/endtext.bvm", "r") as c
+a = open("proyects/" + input("archivo - ") + ".bv", "r")
+exec(a.read())
+c = open("changefiles/endtext.bvm", "r")
 print(c.read())
 tm.sleep(3)
